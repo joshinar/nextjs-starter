8b4f6e664ec8352d76fe06bcd0ef1b7c0b1f45fc
Narendra Joshi <48818291+joshinar@users.noreply.github.com>
2021-06-28 13:34:10 +0530

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,21 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const { Client } = require('pg');
-
-const connectionString = 'postgresql://postgres:7303@localhost:5432/test';
-
-const client = new Client({
-  connectionString,
-});
-client.connect();
 const getAllEmployees = async (req, res) => {
-  try {
-    const data = await client.query('select * from employees limit $1', [
-      req.query.limit,
-    ]);
-    res.json(data.rows);
-  } catch (error) {
-    console.log(error.message);
-  }
+  res.status(200).json({"name":"naren"})
 };
 export default getAllEmployees;
